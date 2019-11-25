619ada7d3f0f7933e695baac6852a90cf8d502a4
Junting0211 <jchen126@u.rochester.edu>
2019-11-25 17:45:35 -0500

    add files

---
+++ member.php
@@ -0,0 +1,34 @@
+<?php 
+$currentPage = "Assignment06 | Team-Seattle";
+include "inc/html-top.inc";
+?>
+<?php include "inc/nav.inc"; ?>
+
+<body>
+
+<h1>E-board members</h1>
+
+<section>
+
+	<h2>Current Members</h2>
+	<p>Currently, URCSSA is extended to 67 members, including 53 undergraduates, 12 graduate students, and 2 doctoral students. In our team, there are both experienced senior students and energetic students. With the joint efforts of new and old members, URCSSA will better accompany everyone to grow together! Regardless of academic, job search or after-school life, we will always keep in mind the glorious mission of serving Chinese students, never forget the original intention, and move forward.</p>
+</section>
+
+<section>
+	<h3>Current Members of OBOC E-Board</h3>
+	<ul>
+		<li>Yuchen Du - President</li>
+		<li>Yiwei Zhou - Vice President</li>
+		<li>Can Wang - Vice President</li>
+		<li>Fangzhou Liu - ASE Director</li>
+		<li>Jinwen Ouyang - Secretary</li>
+		<li>Tianyi Liang - Head of Career Development Department</li>
+		<li>Haosong Zheng - Head of Planning and Strategy Department</li>
+		<li>Yuhan Liu - Head of Communication Department</li>
+		
+	</ul>
+</section>
+
+
+</body>
+</html>
