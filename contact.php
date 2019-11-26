dfb6152497f3c2ceac06d7d3eb4cdc28ae8a2f65
Junting0211 <jchen126@u.rochester.edu>
2019-11-25 20:33:21 -0500

    add thank you

--- contact.php
+++ contact.php
@@ -7,15 +7,21 @@ include "inc/html-top.inc";
 
 <?php include "inc/nav.inc"; ?>
 
-<form action="#" method="post" id="signup">
-	<h1>Contact Us</h1>
+	<section class="newslettersub">
+
+		<div id="newsletter">
+		 <h1>Contact Us</h1>
 	<h2>Feel free to leave a message! We will get back to you ASAP.</h2>
-	<input type="text" placeholder="Email address" name="email" required>
-	<label>
-		<input type="checkbox" checked="checked" name="subscribe"> 
-	</label>
-		<input type="submit" value="Subscribe">
-</form>
+		 
+		 	<form action="#" method="post" id="signup">
+		    <input type="text" placeholder="Email address" name="email" required>
+		    <label>
+		      <input type="checkbox" checked="checked" name="subscribe"> 
+		    </label>
+		    <input type="submit" value="Subscribe">
+		 	</form>
+		</div>
+	</section>
 
 <footer class="footerindex">CSC 174: Advanced Front-end Web Design and Development</footer>
 
